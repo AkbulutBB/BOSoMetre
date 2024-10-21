b9b0c73ee69f8732c9355cfdd0e6fc47ac6c8ec4
B. Bahadır Akbulut, MD. <b.bahadirakbulut@gmail.com>
2024-10-21 15:06:29 +0300

    no need for serial since EEPROM

--- NewDesign/BOSOMetreWatchDog/BOSOMetreWatchDog.ino
+++ NewDesign/BOSOMetreWatchDog/BOSOMetreWatchDog.ino
@@ -71,10 +71,10 @@ void setup() {
 
   // Initialize serial communication
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  Serial.println("Serial communication initialized.");
+ // while (!Serial) {
+ //   ; // wait for serial port to connect. Needed for native USB port only
+ // }
+ // Serial.println("Serial communication initialized.");
 
   // Set up the sensor pins
   setupSensorPins();
